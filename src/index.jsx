762c2a451b32a97a8c4e8f50999a931e823a74d4
hakobya4 <108638724+hakobya4@users.noreply.github.com>
2023-09-07 13:23:46 -0400

    added redux and parcel for netlify

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import { createRoot } from "react-dom";
 import MainView from "../components/main-view/main-view";
+import { Provider } from 'react-redux'
 import "./index.scss";
 import Container from 'react-bootstrap/Container';
 
+
 const App = () => {
   return( 
-  <Container >
+  <Provider store={store} >
     <MainView />
-  </Container>
+  </Provider>
 )};
 
 const container = document.querySelector("#root");
