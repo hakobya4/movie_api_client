9d7d77c10a981af7dcdfcd5af18c06177183f4ab
hakobya4 <108638724+hakobya4@users.noreply.github.com>
2023-10-04 13:12:16 -0400

    Style change

--- components/profile-view/profile-view.jsx
+++ components/profile-view/profile-view.jsx
@@ -6,62 +6,69 @@ import { MovieCard } from "../movie-card/movie-card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-export const ProfileView = ({user, token, onLoggedOut, setUser, movies}) => {
-    const [username, setUsername] = useState(user.Username);
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState(user.Email);
-    const [birthday, setBirthday] = useState(user.Birthday);
-    const favoriteMovies = movies.filter((movie) => {
-        return user.FavoriteMovies.includes(movie.id)
-    })
-    
-    const handleUpdate = (event) => {
-    
+export const ProfileView = ({ user, token, onLoggedOut, setUser, movies }) => {
+  const [username, setUsername] = useState(user.Username);
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState(user.Email);
+  const [birthday, setBirthday] = useState(user.Birthday);
+  const favoriteMovies = movies.filter((movie) => {
+    return user.FavoriteMovies.includes(movie.id);
+  });
+
+  const handleUpdate = (event) => {
     event.preventDefault();
 
     const data = {
       Username: username,
       Password: password,
       Email: email,
-      Birthday: birthday
+      Birthday: birthday,
     };
 
-    fetch(`https://movie-api-myflix-39dfea723223.herokuapp.com/users/${user.Username}`, {
-            method: "PUT",
-            body: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json", Authorization: `Bearer ${token}`
-            }
-          }).then((response) => {
-            if (response.ok) {
-              alert("Success")
-                return response.json()
-            } else {
-                alert("Changes failed")
-            }
-        }).then((data) => {
-            localStorage.setItem("user", JSON.stringify(data));
-            setUser(data);
-        })
-    };
+    fetch(
+      `https://movie-api-myflix-39dfea723223.herokuapp.com/users/${user.Username}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          alert("Success");
+          return response.json();
+        } else {
+          alert("Changes failed");
+        }
+      })
+      .then((data) => {
+        localStorage.setItem("user", JSON.stringify(data));
+        setUser(data);
+      });
+  };
 
-    const handleDeregister = ()=>
-
-    fetch(`https://movie-api-myflix-39dfea723223.herokuapp.com/users/${user.Username}`, {
+  const handleDeregister = () =>
+    fetch(
+      `https://movie-api-myflix-39dfea723223.herokuapp.com/users/${user.Username}`,
+      {
         method: "DELETE",
-            headers: { Authorization: `Bearer ${token}` }
-            }).then((response) => {
-            if (response.ok) {
-                alert("Account deleted");
-                onLoggedOut()
-            } else {
-                alert("Account deletion failed");
-            }
-        });
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    ).then((response) => {
+      if (response.ok) {
+        alert("Account deleted");
+        onLoggedOut();
+      } else {
+        alert("Account deletion failed");
+      }
+    });
 
   return (
     <>
-      <Form className = "profile" onSubmit={handleUpdate}>
+      <Form className="profile" onSubmit={handleUpdate}>
         <Form.Group controlId="formUsername">
           <h3 className="text-light">Change User Information:</h3>
           <Form.Label className="text-light">New Username:</Form.Label>
@@ -69,7 +76,7 @@ export const ProfileView = ({user, token, onLoggedOut, setUser, movies}) => {
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            minLength="5" 
+            minLength="5"
           />
         </Form.Group>
 
@@ -105,17 +112,17 @@ export const ProfileView = ({user, token, onLoggedOut, setUser, movies}) => {
         </Button>
       </Form>
       <Form onClick={handleDeregister}>
-        <Button  className="my-3" variant="primary">
+        <Button className="my-3" variant="primary">
           Deregister
         </Button>
       </Form>
-      <Row >
-            <h1 className="text-light">Favorite movies:</h1>
-            {favoriteMovies.map((movie) => (
-                <Col className="mb-5" key={movie.id} md={12} >
-                    <MovieCard movie={movie}></MovieCard>
-                </Col>
-            ))}
+      <Row>
+        <h1 className="text-light">Favorite movies:</h1>
+        {favoriteMovies.map((movie) => (
+          <Col className="mb-5" key={movie.id} md={12}>
+            <MovieCard movie={movie}></MovieCard>
+          </Col>
+        ))}
       </Row>
     </>
   );
